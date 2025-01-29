33684e88b159cb23b6927adfee0fcf52750fb785
Jetsada123 <rov96832@gmail.com>
2025-01-30 03:46:36 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = function getConfig(deviceId, deviceChange, fbToken) {
+  return {
+  "Device[udid]": deviceId,
+  API_KEY: "viki",
+  API_SECRET: "coin",
+  "Device[change]": deviceChange,
+  fbToken: fbToken,
+  locale: "en",
+  "Device[os]": "WebGL",
+  "Client[version]": "3.5.165"
+}}
+
